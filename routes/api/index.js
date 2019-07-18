8636142a8fa4e59873d108fd45fe9cb8403fc68f
Manzell <manzellhatten@gmail.com>
2019-07-17 23:05:26 -0400

    Thank GOD

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const movieRoutes = require("./movie");
 
-router.use("/movies", movieRoutes);
+router.use("/movie", movieRoutes);
 
 module.exports = router;
