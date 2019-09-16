1786c43178414b14a4d9ecf2bb0aaa2e15f280e2
Manzell <manzellhatten@gmail.com>
2019-09-16 19:20:31 -0400

    With help I was able to make both API calls I need to pratice aysnc and wait though the current code works

---
+++ client/src/components/MovieDetails/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MovieDetails(props) {
+    return (
+        <div className="text-center">
+            <img src={props.src} alt={props.title} style={{ margin: "0 auto" }} />
+            <h3>Genre: {props.genre} </h3>
+            <h3>Released: {props.released} </h3>
+            <h3>Director: {props.director} </h3>
+            <h3>Plot: {props.plot} </h3>
+            <h3>Rating: {props.rating} </h3>
+        </div>
+    )
+}
+
+export default MovieDetails;
--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,58 +1,108 @@
 import React, { Component } from "react"
 import Jumbotron from "../components/Jumpbotron"
+import Card from "../components/Card"
 import { Col, Container, Row } from "../components/Grid"
 import API from "../utils/API"
 
 class MovieDetails extends Component {
-    state = {
-        currentMovie: {},
-        OMDBresult: {}
-    }
+       state = {
+            currentMovie: '',
+            OMDBresult: '',
+
+            // currentMovieTitle: '',
+            // genre: '',
+            // OMDBresultTitle: ''
+        }
+
 
     //Component did mount here for the making the call to database 
     //then storing promise into currentMovie state
     //Once stored make another API Call to IMDB with the title from
     //currentMovie object then make the AJAX request to display info
     //from IMDB
+
+    // async componentDidMount() {
+
+    //     const {data: { genre, title}} = await this.movieDB();
+    //     const { data: { title: OMDBresultTitle }} = await this.movieBE(title)
+
+    //     this.setState({
+    //         currentMovieTitle: title,
+    //         genre,
+    //         OMDBresultTitle
+    //     })
+    // }
+
+    // movieDB = () => API.getMovie(this.props.match.params.id)
+
+    // movieBE = (title) => API.search(title)
+
     componentDidMount() {
         API.getMovie(this.props.match.params.id)
-            .then(res => this.setState({ currentMovie: res.data }))
-            .catch(err =>  console.log(err));
-        
-        const searchedMovie = this.state.currentMovie.title
-        console.log(this.state.currentMovie.title)
-        
-        this.searchMovie(searchedMovie)
-        // searchMovie = query => {
-        //     API.search(this.state.currentMovie.title)
-        //         .then(res => this.setState({ OMDBresult: res.data }))
-        //         .catch(err => console.log(err));
-        // }
+        .then(res => { 
+            this.setState({ 
+                currentMovie: res.data
+            }, () => API.search(this.state.currentMovie.title)
+                .then(res => {
+                    console.log(res.data)
+                    this.setState({
+                        OMDBresult: res.data
+                    })
+                }))
+        })
+        .catch(err => console.log(err));
     }
-        
-        
-        searchMovie = query => {
-            console.log(this.state.currentMovie.title)
-            API.search(query)
-                .then(res => this.setState({ OMDBresult: res.data }))
-                .catch(err => console.log(err));
-        }
 
-    render () {
+    // movieDB = () => {
+    //     API.getMovie(this.props.match.params.id)
+    //     .then(res => { 
+    //         this.setState({ 
+    //             currentMovie: res.data
+    //         }, this.movieDE(this.state.currentMovie.title))
+    //     })
+    //     .catch(err => console.log(err));
+    //     console.log(this.state.currentMovie.title)
+    // }
+
+    // movieDE = (title) => {
+    //     API.search(title)
+    //     .then(res => { 
+    //         this.setState({ 
+    //             OMDBresult: res.data
+    //         })
+    //     })
+    //     .catch(err => console.log(err));
+    // }
+
+
+    render() {
         //Think about how you want all the information displayed below
         //You have the title, poster, director, genre, released date, etc
         //You might be able to pull in a description as well
         //Look at how the books detail page looks and the omdb cards look
         //organize your thoughts and think about how the layout should
+
         return (
             <Container>
                 <Row>
                     <Col>
                         <Jumbotron>
-                            <h1>
-                                {this.state.currentMovie.title} Genre: {this.state.currentMovie.genre}
-                            </h1>
+                                {/* {this.state.currentMovie.title} Genre: {this.state.currentMovie.genre} */}
+                            <Card 
+                            title={this.state.currentMovie.title}
+                            src={this.state.OMDBresult.Poster}
+                            genre={this.state.OMDBresult.Genre}
+                            released={this.state.OMDBresult.Released}
+                            director={this.state.OMDBresult.Director}
+                            plot={this.state.OMDBresult.Plot}
+                            rating={this.state.OMDBresult.imdbRating}
+                            />
                         </Jumbotron>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+
                     </Col>
                 </Row>
             </Container>
